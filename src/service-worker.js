8f1a2e9da36d555a2cff2c2135ce8d60ed389190
tropicbliss <tropicbliss@protonmail.com>
2022-05-31 18:15:33 +0800

    Remove unneeded code

--- src/service-worker.js
+++ src/service-worker.js
@@ -62,13 +62,5 @@ registerRoute(
   })
 );
 
-// This allows the web app to trigger skipWaiting via
-// registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
 // Any other custom service worker logic can go here.
 self.skipWaiting();
