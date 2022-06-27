4a3524ab0b59e38bbea5c1e3c8da162ee61a6423
tropicbliss <tropicbliss@protonmail.com>
2022-06-27 18:48:11 +0800

    E

--- src/components/hardmode/HardModeSwitch.js
+++ src/components/hardmode/HardModeSwitch.js
@@ -3,7 +3,7 @@ import SettingsToggle from "./SettingsToggle";
 
 const HardModeSwitch = ({ toggleHardMode, hardMode }) => {
   return (
-    <div className="flex justify-end mx-3 my-6 space-x-3 items-center dark:text-white">
+    <div className="flex justify-end mx-3 mb-6 mt-3 space-x-3 items-center dark:text-white">
       <div>Hard Mode</div>
       <SettingsToggle handleFlag={toggleHardMode} flag={hardMode} />
     </div>
